7657c178750e68572bb478eb64127b27c53d7e76
justinDev91 <justin@sustainsoft.eu>
2023-09-22 19:37:58 +0200

    feat(back): init spring boot appl and expose api/ endpoint

---
+++ src/main/java/com/example/salineroyaleacademy/SalineRoyaleAcademyApplication.java
@@ -0,0 +1,13 @@
+package com.example.salineroyaleacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalineRoyaleAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalineRoyaleAcademyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/salineroyaleacademy/SalineRoyaleAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.salineroyaleacademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalineRoyaleAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
